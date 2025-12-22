e1658954fffb5f450c14fc4c43ad334256d62836
lolo <234105833+chango112595-cell@users.noreply.github.com>
2025-12-22 02:13:22 -0500

    Add AEM manifest and remove cleanup pass stubs

--- aurora_nexus_v3/generated_modules/validator/validator_0295_cleanup.py
+++ aurora_nexus_v3/generated_modules/validator/validator_0295_cleanup.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 class Validator0295Cleanup:
     def __init__(self):
-        pass
+        self.resource = None
 
     def teardown(self) -> dict:
         try:
