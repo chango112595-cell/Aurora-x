2fecaa9f26bd5c283a5a5b147331af6b013eda06
chango112595-cell <chango112595-cell@users.noreply.github.com>
2026-01-01 09:31:06 -0500

    chore(ci,git): tighten E2E; ignore artifacts & secrets; add health test

--- .next/dev/static/chunks/app_page_tsx_5010fe15._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_ca9d8155._.js",
-  "static/chunks/node_modules_d6b2c290._.js"
-],
-    source: "dynamic"
-});
