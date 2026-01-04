5e4e272165a10a9440bb43e6287e5b12dab3f3d8
chango112595-cell <chango112595-cell@users.noreply.github.com>
2026-01-04 06:07:10 -0500

    chore(js-lint): add ESLint + Prettier and CI workflow

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2022: true },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "import"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  rules: {
+    "import/order": ["error", { "newlines-between": "always", "alphabetize": { "order": "asc" } }]
+  },
+  ignorePatterns: [
+    "node_modules/", "dist/", "build/", ".next/", "client/.next/",
+    "client/dist/", "k8s/", ".aurora/", ".github/", "**/*.d.ts"
+  ],
+  overrides: [{ files: ["**/*.js"], parser: "espree" }]
+};
